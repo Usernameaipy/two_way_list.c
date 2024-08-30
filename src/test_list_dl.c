1b44bb1d099978473d519b5b7c9055b78b499b4a
moon <undruee@gmail.com>
2024-08-30 12:56:34 +0300

    test

---
+++ src/test_list_dl.c
@@ -0,0 +1,79 @@
+#include "test_list_dl.h"
+
+void success(void){
+    printf("SUCCESS\n");
+}
+
+void fail(void){
+    printf("FAIL\n");
+}
+
+void test_init(int var){
+    printf("TEST_INIT\n");
+    struct node* check=init(var);
+    if(check->variable==var && check->next==NULL && check->pref==NULL){
+        success();
+    }
+    else fail();
+    delete_list(check);
+}
+
+void test_insert(int var){
+    printf("TEST_INSERT\n");
+    struct node* root=init(9);
+    print_next(root);
+    insert(root, var);
+    print_next(root);
+    struct node* root_cp=root->next;
+    if(root_cp->variable==var && root_cp->next==NULL && root_cp->pref!=NULL){
+        success();
+    }
+    else fail();
+    delete_list(root);
+}
+
+void test_insert_after(int var){
+    printf("TEST_INSERT_AFTER\n");
+    struct node* root=init(9);
+    insert(root, 2);
+    print_next(root);
+    insert_after(root, root, var);
+    print_next(root);
+    struct node* root_cp=root->next;
+    if(root_cp->variable==var && root_cp->pref==root && root_cp->next!=NULL){
+        success();
+    }
+    else fail();
+    delete_list(root);
+}
+
+void test_insert_before(int var){
+    printf("TEST_INSERT_BEFORE\n");
+    struct node* root=init(9);
+    insert(root, 7);
+    print_next(root);
+    insert_before(root, root->next, var);
+    print_next(root);
+    struct node* root_cp=root->next;
+    if(root_cp->variable==var && root_cp->pref==root && root_cp->next!=NULL){
+        success();
+    }
+    else fail();
+    delete_list(root);
+}
+
+int main(){
+    test_init(TEST_INIT_ONE);
+    test_init(TEST_INIT_TWO);
+    test_init(TEST_INIT_THREE);
+    test_insert(TEST_INSERT_ONE);
+    test_insert(TEST_INSERT_TWO);
+    test_insert(TEST_INSERT_THREE);
+    test_insert_after(TEST_INSERT_AFTER_ONE);
+    test_insert_after(TEST_INSERT_AFTER_TWO);
+    test_insert_after(TEST_INSERT_AFTER_THREE);
+    test_insert_before(TEST_INSERT_BEFORE_ONE);
+    test_insert_before(TEST_INSERT_BEFORE_TWO);
+    test_insert_before(TEST_INSERT_BEFORE_THREE);
+    return 0;
+}
---
+++ src/test_list_dl.h
@@ -0,0 +1,22 @@
+#ifndef TEST_LIST_DL_H
+#define TEST_LIST_DL_H
+#include "list_dl.h"
+void success(void);
+void fail(void);
+void test_init(int);
+void test_insert(int);
+void test_insert_after(int);
+void test_insert_before(int);
+#define TEST_INIT_ONE 8
+#define TEST_INIT_TWO 'c'
+#define TEST_INIT_THREE 72000
+#define TEST_INSERT_ONE 90
+#define TEST_INSERT_TWO 'j'
+#define TEST_INSERT_THREE 72000
+#define TEST_INSERT_AFTER_ONE 45
+#define TEST_INSERT_AFTER_TWO 'h'
+#define TEST_INSERT_AFTER_THREE 72000
+#define TEST_INSERT_BEFORE_ONE 78
+#define TEST_INSERT_BEFORE_TWO 'o'
+#define TEST_INSERT_BEFORE_THREE 72000
+#endif
